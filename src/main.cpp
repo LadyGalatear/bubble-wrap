bc93b440ba4a5efbe765e1381810f6303db8806e
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-01-15 04:03:45 +0000

    Added b/g change

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <bn_core.h>
+#include <bn_keypad.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
 
@@ -8,6 +9,10 @@ int main() {
     bn::backdrop::set_color(bn::color(20, 20, 31));
 
     while(true) {
+        if (bn::keypad::a_pressed()) {
+            bn::backdrop::set_color(bn::color(31, 20, 20));
+        }
+
         bn::core::update();
     }
 }
