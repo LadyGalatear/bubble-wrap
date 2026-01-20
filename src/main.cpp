2498a4db56db2973968b68e739bcdfe21b8dc60f
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-01-20 06:24:57 +0000

    Add sprites

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,42 @@
 #include <bn_keypad.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
+#include <bn_sprite_ptr.h>
+#include <bn_sprite_items_dot.h>
 
-int main() {
+int main()
+{
     bn::core::init();
 
     bn::backdrop::set_color(bn::color(20, 20, 31));
 
-    while(true) {
-        if (bn::keypad::a_pressed()) {
+    bn::sprite_ptr myCircle0 = bn::sprite_items::dot.create_sprite(10, 40);
+    bn::sprite_ptr myCircle1 = bn::sprite_items::dot.create_sprite(-10, 40);
+    bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-30, 40);
+    bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(30, 40);
+
+    while (true)
+    {
+        if (bn::keypad::a_pressed())
+        {
             bn::backdrop::set_color(bn::color(31, 20, 20));
         }
 
-        if (bn::keypad::b_pressed()) {
+        if (bn::keypad::b_pressed())
+        {
             bn::backdrop::set_color(bn::color(20, 31, 20));
         }
 
-        if (bn::keypad::l_held() && bn::keypad::r_held()) {
+        if (bn::keypad::l_held() && bn::keypad::r_held())
+        {
             bn::backdrop::set_color(bn::color(5, 5, 5));
 
-            for (int i = 0; i < 60; ++i) {
+            for (int i = 0; i < 60; ++i)
+            {
                 bn::core::update();
             }
 
-            bn::backdrop::set_color(bn::color(15, 10, 15));
+            bn::backdrop::set_color(bn::color(31, 20, 31));
         }
 
         bn::core::update();
